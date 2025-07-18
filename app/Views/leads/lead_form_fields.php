a8f6a91aa5b4f0a1edc72342f6613152c4bcc8e9
Ashley Burton <burton.ashley@gmail.com>
2025-07-18 14:24:19 -0300

    revert changes

--- app/Views/leads/lead_form_fields.php
+++ app/Views/leads/lead_form_fields.php
@@ -41,6 +41,7 @@
                     "value" => $model_info->company_name,
                     "class" => "form-control company_name_input_section",
                     "placeholder" => app_lang('company_name'),
+                    "autofocus" => true,
                     "data-rule-required" => true,
                     "data-msg-required" => app_lang("field_required"),
                 ));
@@ -60,6 +61,7 @@
                     "value" => $model_info->company_name,
                     "class" => "form-control company_name_input_section",
                     "placeholder" => app_lang('company_name'),
+                    "autofocus" => true,
                     "data-rule-required" => true,
                     "data-msg-required" => app_lang("field_required"),
                 ));
@@ -121,15 +123,12 @@
         <label for="address" class="<?php echo $label_column; ?>"><?php echo app_lang('address'); ?></label>
         <div class="<?php echo $field_column; ?>">
             <?php
-            echo form_input(array(
+            echo form_textarea(array(
                 "id" => "address",
                 "name" => "address",
                 "value" => $model_info->address ? $model_info->address : "",
                 "class" => "form-control",
-                "placeholder" => app_lang('address'),
-                "autocomplete" => "off",
-                "data-autofill" => "new-address",
-                "aria-autocomplete" => "list"
+                "placeholder" => app_lang('address')
             ));
             ?>
 
@@ -156,17 +155,15 @@
     <div class="row">
         <label for="state" class="<?php echo $label_column; ?>"><?php echo app_lang('state'); ?></label>
         <div class="<?php echo $field_column; ?>">
-            <select id="state" name="state" class="form-control select2" data-rule-required="true" data-msg-required="<?php echo app_lang('field_required'); ?>">
-                <option value="">- Select Province -</option>
-                <option value="New Brunswick" <?php echo $model_info->state == 'New Brunswick' ? 'selected' : ''; ?>>New Brunswick</option>
-                <option value="Nova Scotia" <?php echo $model_info->state == 'Nova Scotia' ? 'selected' : ''; ?>>Nova Scotia</option>
-                <option value="Prince Edward Island" <?php echo $model_info->state == 'Prince Edward Island' ? 'selected' : ''; ?>>Prince Edward Island</option>
-                <option value="Quebec" <?php echo $model_info->state == 'Quebec' ? 'selected' : ''; ?>>Quebec</option>
-                <option value="Ontario" <?php echo $model_info->state == 'Ontario' ? 'selected' : ''; ?>>Ontario</option>
-                <option value="Manitoba" <?php echo $model_info->state == 'Manitoba' ? 'selected' : ''; ?>>Manitoba</option>
-                <option value="Northwest Territories" <?php echo $model_info->state == 'Northwest Territories' ? 'selected' : ''; ?>>Northwest Territories</option>
-                <option value="British Columbia" <?php echo $model_info->state == 'British Columbia' ? 'selected' : ''; ?>>British Columbia</option>
-            </select>
+            <?php
+            echo form_input(array(
+                "id" => "state",
+                "name" => "state",
+                "value" => $model_info->state,
+                "class" => "form-control",
+                "placeholder" => app_lang('state')
+            ));
+            ?>
         </div>
     </div>
 </div>
--- app/Views/leads/modal_form.php
+++ app/Views/leads/modal_form.php
@@ -11,7 +11,6 @@
 </div>
 <?php echo form_close(); ?>
 
-<!-- Google Maps API Script -->
 <script type="text/javascript">
     $(document).ready(function () {
         $("#lead-form").appForm({
@@ -27,150 +26,8 @@
                 }
             }
         });
-
         setTimeout(function () {
             $("#company_name").focus();
         }, 200);
-
-        // Prevent browser autofill on address field
-        var addressInput = $('#lead-form #address');
-        addressInput.attr('autocomplete', 'new-address');
-        addressInput.on('focus', function () {
-            $(this).attr('autocomplete', 'new-address');
-        });
-
-        // Dynamic logic: override lead source based on location
-        $(document).on("change keyup", "#state, #city", updateLeadSource);
-
-        loadMapsScript(initLeadModalAutocomplete);
     });
-
-    function loadMapsScript(callback) {
-        if (window.google && google.maps && google.maps.places) {
-            callback();
-            return;
-        }
-        var script = document.createElement('script');
-        script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCN7FS848BKLuWUjFlV6c7NKxDlcebCL_g&libraries=places";
-        script.async = true;
-        script.defer = true;
-        script.onload = callback;
-        document.head.appendChild(script);
-    }
-    function updateLeadSource() {
-        var cityVal = $("#city").val().trim().toLowerCase();
-        var provinceVal = $("#state").val();
-
-        if (!provinceVal) {
-            $("#lead_source_id").val("").trigger("change");
-            return;
-        }
-
-        var sourceMap = {
-            "New Brunswick": 1,
-            "Nova Scotia": 1,
-            "Prince Edward Island": 1,
-            "Quebec": 5,
-            "Ontario": 6,
-            "Manitoba": 3,
-            "Northwest Territories": 3,
-            "British Columbia": 3
-        };
-
-        if (provinceVal === "British Columbia") {
-            var bcCitiesKeywords = [
-                "vancouver", "burnaby", "richmond", "surrey", "delta", "new westminster",
-                "langley", "white rock", "maple ridge", "pitt meadows", "coquitlam",
-                "port coquitlam", "port moody", "north vancouver", "west vancouver",
-                "belcarra", "anmore", "bowen island", "lions bay",
-                "abbotsford", "chilliwack", "mission", "kent", "agassiz",
-                "harrison hot springs", "hope",
-                "sechelt", "gibsons",
-                "victoria", "saanich", "oak bay", "esquimalt", "view royal", "colwood",
-                "langford", "metchosin", "highlands", "sooke", "central saanich", "north saanich",
-                "sidney", "duncan", "north cowichan", "lake cowichan", "ladysmith", "nanaimo",
-                "lantzville", "parksville", "qualicum beach", "port alberni", "tofino",
-                "ucluelet", "courtenay", "comox", "cumberland", "campbell river", "gold river",
-                "tahsis", "zeballos", "port hardy", "port mcneill", "port alice", "alert bay"
-            ];
-
-            for (var i = 0; i < bcCitiesKeywords.length; i++) {
-                if (cityVal.indexOf(bcCitiesKeywords[i]) !== -1) {
-                    $("#lead_source_id").val("2").trigger("change");
-                    return;
-                }
-            }
-            $("#lead_source_id").val("3").trigger("change");
-            return;
-        }
-
-        var mappedId = sourceMap[provinceVal] ? sourceMap[provinceVal] : "";
-        $("#lead_source_id").val(mappedId).trigger("change");
-    }
-
-    // Google Places Autocomplete Initialization
-    function initLeadModalAutocomplete() {
-        var addressInput = document.querySelector('#lead-form #address');
-        if (!addressInput) {
-            return;
-        }
-        var autocomplete = new google.maps.places.Autocomplete(addressInput, {
-            types: ['address'],
-            componentRestrictions: { country: ['ca'] },
-            fields: ['address_components', 'geometry', 'formatted_address']
-        });
-
-        autocomplete.addListener('place_changed', function () {
-            var place = autocomplete.getPlace();
-            var addressComponents = place.address_components;
-
-            $('#lead-form #address').val('');
-            $('#lead-form #city').val('');
-            $('#lead-form #state').val('');
-            $('#lead-form #zip').val('');
-            $('#lead-form #country').val('');
-
-            var streetNumber = '';
-            var route = '';
-            var city = '';
-            var province = '';
-            var postalCode = '';
-            var country = '';
-
-            for (var i = 0; i < addressComponents.length; i++) {
-                var component = addressComponents[i];
-                var types = component.types;
-
-                if (types.includes('street_number')) {
-                    streetNumber = component.short_name;
-                }
-                if (types.includes('route')) {
-                    route = component.long_name;
-                }
-                if (types.includes('locality')) {
-                    city = component.long_name;
-                }
-                if (types.includes('administrative_area_level_1')) {
-                    province = component.long_name;
-                }
-                if (types.includes('postal_code')) {
-                    postalCode = component.short_name;
-                }
-                if (types.includes('country')) {
-                    country = component.long_name;
-                }
-            }
-
-            var fullAddress = streetNumber && route ? streetNumber + ' ' + route : route;
-            $('#lead-form #address').val(fullAddress);
-            $('#lead-form #city').val(city);
-            $('#lead-form #state').val(province).trigger('change');
-            $('#lead-form #zip').val(postalCode);
-            $('#lead-form #country').val(country);
-
-            updateLeadSource();
-        });
-    }
-</script>
-
-
+</script>    
