a1d5f3383c0e3f3e701f45633681c0de3557b3c2
Ashley Burton <burton.ashley@gmail.com>
2025-09-02 19:36:14 -0300

    Add comments and PDF export to NPS report

--- plugins/Polls/Views/nps/report.php
+++ plugins/Polls/Views/nps/report.php
@@ -2,6 +2,11 @@
     <div class="card">
         <div class="page-title clearfix">
             <h1><?php echo $survey->title; ?></h1>
+            <?php if (!isset($is_pdf)) { ?>
+                <div class="title-button-group">
+                    <?php echo anchor(get_uri("nps/download_pdf/" . $survey->id), "<i data-feather='download' class='icon-16'></i> " . app_lang('download_pdf'), array("class" => "btn btn-default")); ?>
+                </div>
+            <?php } ?>
         </div>
         <div class="p15">
             <div class="mb15">
@@ -37,7 +42,9 @@
 
             <p class="mt20"><strong><?php echo app_lang('nps_score'); ?>:</strong> <?php echo round($nps_score, 2); ?></p>
 
-            <?php echo view("Polls\\Views\\polls\\vote_pie_chart", array("poll_answers" => $poll_answers)); ?>
+            <?php if (!isset($is_pdf)) {
+                echo view("Polls\\Views\\polls\\vote_pie_chart", array("poll_answers" => $poll_answers));
+            } ?>
 
             <?php if (!empty($questions_summary)) { ?>
                 <div class="mt30">
@@ -96,7 +103,29 @@
 
                         <p class="mt20"><strong><?php echo app_lang('nps_score'); ?>:</strong> <?php echo round($question['nps_score'], 2); ?></p>
 
-                        <?php echo view("Polls\\Views\\polls\\vote_pie_chart", array("poll_answers" => $question['poll_answers'], "chart_id" => "vote-status-chart-" . $question['id'])); ?>
+                        <?php if (!isset($is_pdf)) {
+                            echo view("Polls\\Views\\polls\\vote_pie_chart", array("poll_answers" => $question['poll_answers'], "chart_id" => "vote-status-chart-" . $question['id']));
+                        } ?>
+
+                        <?php if (isset($comments_by_question[$question['id']])) { ?>
+                            <h5 class="mt30"><?php echo app_lang('comments'); ?></h5>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th><?php echo app_lang('score'); ?></th>
+                                        <th><?php echo app_lang('comment'); ?></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($comments_by_question[$question['id']] as $response) { ?>
+                                        <tr>
+                                            <td><?php echo $response->score; ?></td>
+                                            <td><?php echo nl2br($response->comment); ?></td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        <?php } ?>
                     <?php } ?>
                 </div>
             <?php } ?>
