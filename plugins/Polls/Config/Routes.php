a9cf5866adc21033a03776e836aced9cc5cb36a2
Ashley Burton <burton.ashley@gmail.com>
2025-09-02 18:17:27 -0300

    Fix NPS embed 404 by reordering routes

--- plugins/Polls/Config/Routes.php
+++ plugins/Polls/Config/Routes.php
@@ -11,12 +11,15 @@ $routes->post('polls/(:any)', 'Polls::$1', $polls_namespace);
 $routes->get('polls/(:any)', 'Polls::$1', $polls_namespace);
 
 $routes->get('nps', 'Nps::index', $polls_namespace);
+
+// public routes should be defined before catch-all to prevent overrides
+$routes->get('nps/s/(:num)', 'Nps_public::view/$1', $polls_namespace);
+$routes->get('nps/embed/(:num)', 'Nps_public::embed/$1', $polls_namespace);
+$routes->post('nps/submit', 'Nps_public::submit', $polls_namespace);
+
+// internal NPS routes
 $routes->post('nps/(:any)', 'Nps::$1', $polls_namespace);
 $routes->get('nps/(:any)', 'Nps::$1', $polls_namespace);
-
-$routes->get('nps/s/(:num)', 'Nps_public::view/$1', $polls_namespace);
-$routes->post('nps/submit', 'Nps_public::submit', $polls_namespace);
-$routes->get('nps/embed/(:num)', 'Nps_public::embed/$1', $polls_namespace);
 
 $routes->get('poll_settings', 'Poll_settings::index', $polls_namespace);
 $routes->post('poll_settings/(:any)', 'Poll_settings::$1', $polls_namespace);
