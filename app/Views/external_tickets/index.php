590fc7aba122e9e35c0113d98a3de5ae93c762ea
Ashley Burton <burton.ashley@gmail.com>
2025-10-01 15:14:55 -0300

    Render select custom fields in HTML embed

--- app/Views/external_tickets/index.php
+++ app/Views/external_tickets/index.php
@@ -90,20 +90,37 @@ table.dataTable tbody td:first-child {
 <div id="page-content" class="page-wrapper clearfix">
     <div id="external-ticket-form-container">
 
-        <?php echo form_open(get_uri("external_tickets/save"), array("id" => "ticket-form", "class" => "general-form", "role" => "form")); ?>
+        <?php
+        $phone_minlength_message = app_lang('phone_minlength_error');
+        if ($phone_minlength_message === 'phone_minlength_error') {
+            $phone_minlength_message = "Phone number must be at least 10 digits.";
+        }
+
+        echo form_open(get_uri("external_tickets/save"), array("id" => "ticket-form", "class" => "general-form", "role" => "form"));
+        ?>
         <div id="new-ticket-dropzone" class="card p15 no-border clearfix post-dropzone client-info-section" style="max-width: 100%; margin: auto;">
 
 
+            <input type="hidden" name="is_embedded_form" value="1" />
+            <input type="hidden" name="redirect_to" value="https://www.avenirenergy.ca/avenir-energy-thank-you" />
+            <?php
+            $default_assignee_id = !empty($selected_assignee_id) ? htmlspecialchars($selected_assignee_id) : "";
+            ?>
+            <input type="hidden" name="assigned_to" id="assigned_to" value="<?php echo $default_assignee_id; ?>" data-default-value="<?php echo $default_assignee_id; ?>" />
+            <?php if (!empty($selected_label_ids)) { ?>
+                <input type="hidden" name="labels" value="<?php echo htmlspecialchars($selected_label_ids); ?>" />
+            <?php } ?>
+
             <div class="form-group">
-                <label for="title"><?php echo app_lang('title'); ?></label>
+                <label for="first_name"><?php echo app_lang('first_name'); ?>*</label>
                 <div>
                     <?php
                     echo form_input(array(
-                        "id" => "title",
-                        "name" => "title",
+                        "id" => "first_name",
+                        "name" => "first_name",
                         "value" => "",
                         "class" => "form-control",
-                        "placeholder" => app_lang('title'),
+                        "placeholder" => app_lang('first_name'),
                         "autofocus" => true,
                         "data-rule-required" => true,
                         "data-msg-required" => app_lang("field_required"),
@@ -113,22 +130,45 @@ table.dataTable tbody td:first-child {
             </div>
 
             <div class="form-group">
-                <label for="ticket_type_id"><?php echo app_lang('ticket_type'); ?></label>
+                <label for="last_name"><?php echo app_lang('last_name'); ?>*</label>
                 <div>
                     <?php
-                    echo form_dropdown("ticket_type_id", $ticket_types_dropdown, "", "class='select2'");
+                    echo form_input(array(
+                        "id" => "last_name",
+                        "name" => "last_name",
+                        "value" => "",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('last_name'),
+                        "data-rule-required" => true,
+                        "data-msg-required" => app_lang("field_required"),
+                    ));
                     ?>
                 </div>
             </div>
-    <div class="form-group">
-                <label for="email"><?php echo app_lang('your_email'); ?></label>
+
+            <div class="form-group">
+                <label for="company_name"><?php echo app_lang('company_name'); ?></label>
+                <div>
+                    <?php
+                    echo form_input(array(
+                        "id" => "company_name",
+                        "name" => "company_name",
+                        "value" => "",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('company_name')
+                    ));
+                    ?>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="email"><?php echo app_lang('email'); ?>*</label>
                 <div>
                     <?php
                     echo form_input(array(
                         "id" => "email",
                         "name" => "email",
                         "class" => "form-control p10",
-                        "autofocus" => true,
                         "placeholder" => app_lang('email'),
                         "data-rule-email" => true,
                         "data-msg-email" => app_lang("enter_valid_email"),
@@ -140,22 +180,142 @@ table.dataTable tbody td:first-child {
             </div>
 
             <div class="form-group">
-                <label for="name"><?php echo app_lang('your_name'); ?></label>
+                <label for="address"><?php echo app_lang('address'); ?>*</label>
                 <div>
                     <?php
                     echo form_input(array(
-                        "id" => "name",
-                        "name" => "name",
-                        "value" => "",
+                        "id" => "address",
+                        "name" => "address",
                         "class" => "form-control",
-                        "placeholder" => app_lang('name'),
+                        "placeholder" => app_lang('address'),
+                        "autocomplete" => "off",
+                        "data-autofill" => "new-address",
+                        "aria-autocomplete" => "list",
+                        "data-rule-required" => true,
+                        "data-msg-required" => app_lang("field_required"),
                     ));
                     ?>
                 </div>
             </div>
-      
 
-            <?php echo view("custom_fields/form/prepare_context_fields", array("custom_fields" => $custom_fields, "label_column" => "", "field_column" => "")); ?> 
+            <div class="form-group">
+                <label for="city"><?php echo app_lang('city'); ?>*</label>
+                <div>
+                    <?php
+                    echo form_input(array(
+                        "id" => "city",
+                        "name" => "city",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('city'),
+                        "data-rule-required" => true,
+                        "data-msg-required" => app_lang("field_required"),
+                    ));
+                    ?>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="state"><?php echo app_lang('state'); ?>*</label>
+                <div>
+                    <select id="state" name="state" class="form-control select2" data-rule-required="true" data-msg-required="<?php echo app_lang('field_required'); ?>">
+                        <option value="">- Select Province -</option>
+                        <option value="New Brunswick">New Brunswick</option>
+                        <option value="Nova Scotia">Nova Scotia</option>
+                        <option value="Prince Edward Island">Prince Edward Island</option>
+                        <option value="Quebec">Quebec</option>
+                        <option value="Ontario">Ontario</option>
+                        <option value="Manitoba">Manitoba</option>
+                        <option value="Northwest Territories">Northwest Territories</option>
+                        <option value="British Columbia">British Columbia</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="zip"><?php echo app_lang('zip'); ?>*</label>
+                <div>
+                    <?php
+                    echo form_input(array(
+                        "id" => "zip",
+                        "name" => "zip",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('zip'),
+                        "data-rule-required" => true,
+                        "data-msg-required" => app_lang("field_required"),
+                    ));
+                    ?>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="lead_source_id"><?php echo app_lang('lead_source'); ?>*</label>
+                <div>
+                    <select
+                        name="lead_source_id"
+                        id="lead_source_id"
+                        class="form-control select2"
+                        data-rule-required="true"
+                        data-msg-required="<?php echo app_lang("field_required"); ?>"
+                    >
+                        <option value="">- Select -</option>
+                        <option value="1">CA_Eastern ROC</option>
+                        <option value="2">CA_Pacific</option>
+                        <option value="3">CA_Prairies</option>
+                        <option value="4">CA_Atlantic</option>
+                        <option value="5">CA_Quebec</option>
+                        <option value="6">CA_Ontario</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="phone"><?php echo app_lang('phone'); ?>*</label>
+                <div>
+                    <?php
+                    echo form_input(array(
+                        "id" => "phone",
+                        "name" => "phone",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('phone'),
+                        "data-rule-required" => true,
+                        "data-msg-required" => app_lang("field_required"),
+                        "data-rule-minlength" => 10,
+                        "data-msg-minlength" => $phone_minlength_message,
+                    ));
+                    ?>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="title"><?php echo app_lang('title'); ?></label>
+                <div>
+                    <?php
+                    echo form_input(array(
+                        "id" => "title",
+                        "name" => "title",
+                        "value" => "",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('title'),
+                    ));
+                    ?>
+                </div>
+            </div>
+
+            <?php if (!empty($selected_ticket_type_id)) { ?>
+                <input type="hidden" name="ticket_type_id" value="<?php echo htmlspecialchars($selected_ticket_type_id); ?>" />
+            <?php } else { ?>
+                <div class="form-group">
+                    <label for="ticket_type_id"><?php echo app_lang('ticket_type'); ?></label>
+                    <div>
+                        <?php
+                        echo form_dropdown("ticket_type_id", $ticket_types_dropdown, "", "class='select2'");
+                        ?>
+                    </div>
+                </div>
+            <?php } ?>
+
+
+            <?php echo view("custom_fields/form/prepare_context_fields", array("custom_fields" => $custom_fields, "label_column" => "", "field_column" => "")); ?>
 
               <div class="form-group">
                 <label for="description"><?php echo app_lang('description'); ?></label>
@@ -207,6 +367,12 @@ table.dataTable tbody td:first-child {
             },
             onSuccess: function (result) {
                 appLoader.hide();
+
+                if (result.redirect_url) {
+                    window.top.location.href = result.redirect_url;
+                    return;
+                }
+
                 $("#external-ticket-form-container").html("");
                 appAlert.success(result.message, {container: "#external-ticket-form-container", animate: false});
                 $('.scrollable-page').scrollTop(0); //scroll to top
@@ -214,9 +380,121 @@ table.dataTable tbody td:first-child {
         });
 
         setTimeout(function () {
-            $("#title").focus();
+            $("#first_name").focus();
         }, 200);
 
         $("#ticket-form .select2").select2();
+
+        var addressInput = $("#address");
+        addressInput.attr('autocomplete', 'new-address');
+        addressInput.on('focus', function () {
+            $(this).attr('autocomplete', 'new-address');
+        });
+
+        var ownerMap = {
+            "2": "254",
+            "3": "253",
+            "4": "251",
+            "5": "3827",
+            "6": "252"
+        };
+
+        var $assignedTo = $("#assigned_to");
+        var defaultAssignee = "";
+        if ($assignedTo.length) {
+            var storedDefault = $assignedTo.attr("data-default-value");
+            defaultAssignee = typeof storedDefault !== "undefined" ? storedDefault : ($assignedTo.val() || "");
+        }
+
+        function updateAssignedOwner(leadSourceId) {
+            if (!$assignedTo.length) {
+                return;
+            }
+
+            var leadSourceKey = leadSourceId ? String(leadSourceId) : "";
+            var mappedOwner = ownerMap.hasOwnProperty(leadSourceKey) ? ownerMap[leadSourceKey] : "";
+            if (mappedOwner) {
+                $assignedTo.val(mappedOwner);
+            } else {
+                $assignedTo.val(defaultAssignee);
+            }
+        }
+
+        $("#state, #city").on("change keyup", updateLeadSource);
+        updateLeadSource();
+
+        function updateLeadSource() {
+            var provinceVal = $("#state").val();
+
+            if (!provinceVal) {
+                $("#lead_source_id").val("").trigger("change");
+                updateAssignedOwner("");
+                return;
+            }
+
+            var sourceMap = {
+                "New Brunswick": 4,
+                "Nova Scotia": 4,
+                "Prince Edward Island": 4,
+                "Quebec": 5,
+                "Ontario": 6,
+                "Manitoba": 3,
+                "Northwest Territories": 3,
+                "British Columbia": 3
+            };
+
+            if (provinceVal === "British Columbia") {
+                var bcCitiesKeywords = [
+                    "vancouver", "burnaby", "richmond", "surrey", "delta", "new westminster",
+                    "langley", "white rock", "maple ridge", "pitt meadows", "coquitlam",
+                    "port coquitlam", "port moody", "north vancouver", "west vancouver",
+                    "belcarra", "anmore", "bowen island", "lions bay",
+                    "abbotsford", "chilliwack", "mission", "kent", "agassiz",
+                    "harrison hot springs", "hope",
+                    "sechelt", "gibsons",
+                    "victoria", "saanich", "oak bay", "esquimalt", "view royal", "colwood",
+                    "langford", "metchosin", "highlands", "sooke", "central saanich", "north saanich",
+                    "sidney", "duncan", "north cowichan", "lake cowichan", "ladysmith", "nanaimo",
+                    "lantzville", "parksville", "qualicum beach", "port alberni", "tofino",
+                    "ucluelet", "courtenay", "comox", "cumberland", "campbell river", "gold river",
+                    "tahsis", "zeballos", "port hardy", "port mcneill", "port alice", "alert bay"
+                ];
+
+                var cityVal = $("#city").val().trim().toLowerCase();
+                var matchedKeyword = bcCitiesKeywords.some(function (keyword) {
+                    return cityVal.indexOf(keyword) !== -1;
+                });
+
+                if (matchedKeyword) {
+                    $("#lead_source_id").val("2").trigger("change");
+                    updateAssignedOwner("2");
+                    return;
+                }
+            }
+
+            var mappedSource = sourceMap[provinceVal] || "";
+            var mappedSourceKey = mappedSource ? String(mappedSource) : "";
+            $("#lead_source_id").val(mappedSourceKey).trigger("change");
+            updateAssignedOwner(mappedSourceKey);
+        }
+
+        $("#lead_source_id").on("change", function () {
+            updateAssignedOwner($(this).val());
+        });
+
+        updateAssignedOwner($("#lead_source_id").val());
     });
 </script>
+
+<?php $googleMapsApiKey = get_setting('google_maps_api_key'); ?>
+<?php if ($googleMapsApiKey) { ?>
+    <script src="<?php echo base_url('assets/js/google_address_autocomplete.js'); ?>"></script>
+    <script>
+        function googleMapsReady() {
+            if (window.initAddressAutocomplete) {
+                window.initAddressAutocomplete(document);
+            }
+        }
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=<?php echo $googleMapsApiKey; ?>&libraries=places&v=beta&callback=googleMapsReady&loading=async"></script>
+<?php } ?>
---
+++ app/Views/external_tickets/ticket_html_form_code.php
@@ -0,0 +1,224 @@
+<form action="<?php echo get_uri("external_tickets/save"); ?>" role="form" method="post" accept-charset="utf-8" id="external-ticket-form">
+
+    <input type="hidden" name="is_embedded_form" value="1" />
+    <input type="hidden" name="redirect_to" value="https://www.avenirenergy.ca/avenir-energy-thank-you" />
+
+    <?php
+    $default_assignee_id = !empty($selected_assignee_id) ? htmlspecialchars($selected_assignee_id) : "";
+    ?>
+    <input type="hidden" name="assigned_to" id="assigned_to" value="<?php echo $default_assignee_id; ?>" data-default-value="<?php echo $default_assignee_id; ?>" />
+
+    <?php if (!empty($selected_label_ids)) { ?>
+        <input type="hidden" name="labels" value="<?php echo htmlspecialchars($selected_label_ids); ?>" />
+    <?php } ?>
+
+    <input type="text" name="first_name" id="first_name" placeholder="<?php echo app_lang('first_name'); ?>" required="required" />
+
+    <input type="text" name="last_name" id="last_name" placeholder="<?php echo app_lang('last_name'); ?>" required="required" />
+
+    <input type="text" name="company_name" id="company_name" placeholder="<?php echo app_lang('company_name'); ?>" />
+
+    <input type="email" name="email" id="email" placeholder="<?php echo app_lang('email'); ?>" autocomplete="off" required="required" />
+
+    <input type="text" name="address" id="address" placeholder="<?php echo app_lang('address'); ?>" autocomplete="off" required="required" />
+
+    <input type="text" name="city" id="city" placeholder="<?php echo app_lang('city'); ?>" required="required" />
+
+    <select name="state" id="state" required="required">
+        <option value="">- Select Province -</option>
+        <option value="New Brunswick">New Brunswick</option>
+        <option value="Nova Scotia">Nova Scotia</option>
+        <option value="Prince Edward Island">Prince Edward Island</option>
+        <option value="Quebec">Quebec</option>
+        <option value="Ontario">Ontario</option>
+        <option value="Manitoba">Manitoba</option>
+        <option value="Northwest Territories">Northwest Territories</option>
+        <option value="British Columbia">British Columbia</option>
+    </select>
+
+    <input type="text" name="zip" id="zip" placeholder="<?php echo app_lang('zip'); ?>" required="required" />
+
+    <select name="lead_source_id" id="lead_source_id" required="required">
+        <option value="">- Select -</option>
+        <option value="1">CA_Eastern ROC</option>
+        <option value="2">CA_Pacific</option>
+        <option value="3">CA_Prairies</option>
+        <option value="4">CA_Atlantic</option>
+        <option value="5">CA_Quebec</option>
+        <option value="6">CA_Ontario</option>
+    </select>
+
+    <input type="tel" name="phone" id="phone" placeholder="<?php echo app_lang('phone'); ?>" minlength="10" required="required" />
+
+    <input type="text" name="title" id="title" placeholder="<?php echo app_lang('title'); ?>" />
+
+    <?php if (!empty($selected_ticket_type_id)) { ?>
+        <input type="hidden" name="ticket_type_id" value="<?php echo htmlspecialchars($selected_ticket_type_id); ?>" />
+    <?php } else { ?>
+        <select name="ticket_type_id" id="ticket_type_id">
+            <option value=""><?php echo "- " . app_lang('ticket_type') . " -"; ?></option>
+            <?php if (!empty($ticket_types)) { foreach ($ticket_types as $type) { ?>
+                <option value="<?php echo htmlspecialchars($type->id); ?>"><?php echo htmlspecialchars($type->title); ?></option>
+            <?php }} ?>
+        </select>
+    <?php } ?>
+
+    <?php if (!empty($custom_fields)) {
+        foreach ($custom_fields as $field) {
+            $field_id = isset($field->id) ? (int) $field->id : 0;
+            if (!$field_id) {
+                continue;
+            }
+
+            $field_title = isset($field->title_language_key) && $field->title_language_key ? app_lang($field->title_language_key) : $field->title;
+            $placeholder_language_key = isset($field->placeholder_language_key) ? $field->placeholder_language_key : "";
+            $placeholder_value = isset($field->placeholder) ? $field->placeholder : "";
+            $placeholder = $placeholder_language_key ? app_lang($placeholder_language_key) : ($placeholder_value ? $placeholder_value : $field_title);
+            $field_type = isset($field->field_type) ? $field->field_type : "";
+            $is_required = !empty($field->required);
+
+            $field_name = "custom_field_" . $field_id;
+            $required_attribute = $is_required ? ' required="required"' : '';
+
+            if ($field_type === "select") {
+                $options = array();
+                if (!empty($field->options)) {
+                    $raw_options = explode(",", $field->options);
+                    foreach ($raw_options as $option) {
+                        $trimmed = trim($option);
+                        if ($trimmed !== "") {
+                            $options[] = $trimmed;
+                        }
+                    }
+                }
+
+                ?>
+                <select name="<?php echo htmlspecialchars($field_name); ?>" id="<?php echo htmlspecialchars($field_name); ?>"<?php echo $required_attribute; ?>>
+                    <option value=""><?php echo htmlspecialchars($placeholder); ?></option>
+                    <?php foreach ($options as $option) { ?>
+                        <option value="<?php echo htmlspecialchars($option); ?>"><?php echo htmlspecialchars($option); ?></option>
+                    <?php } ?>
+                </select>
+                <?php
+                continue;
+            }
+
+            ?>
+            <input type="text" name="<?php echo htmlspecialchars($field_name); ?>" id="<?php echo htmlspecialchars($field_name); ?>" placeholder="<?php echo htmlspecialchars($placeholder); ?>"<?php echo $required_attribute; ?> />
+        <?php }
+    } ?>
+
+    <textarea name="description" id="description" placeholder="<?php echo app_lang('description'); ?>" required="required"></textarea>
+
+    <button type="submit"><?php echo app_lang('submit'); ?></button>
+
+</form>
+
+<script type="text/javascript">
+(function () {
+    var stateField = document.getElementById('state');
+    var cityField = document.getElementById('city');
+    var leadSourceField = document.getElementById('lead_source_id');
+    var assignedField = document.getElementById('assigned_to');
+
+    var defaultAssignee = assignedField ? (assignedField.getAttribute('data-default-value') || assignedField.value || '') : '';
+
+    var ownerMap = {
+        '2': '254',
+        '3': '253',
+        '4': '251',
+        '5': '3827',
+        '6': '252'
+    };
+
+    var sourceMap = {
+        'New Brunswick': '4',
+        'Nova Scotia': '4',
+        'Prince Edward Island': '4',
+        'Quebec': '5',
+        'Ontario': '6',
+        'Manitoba': '3',
+        'Northwest Territories': '3',
+        'British Columbia': '3'
+    };
+
+    var bcCitiesKeywords = [
+        'vancouver', 'burnaby', 'richmond', 'surrey', 'delta', 'new westminster',
+        'langley', 'white rock', 'maple ridge', 'pitt meadows', 'coquitlam',
+        'port coquitlam', 'port moody', 'north vancouver', 'west vancouver',
+        'belcarra', 'anmore', 'bowen island', 'lions bay',
+        'abbotsford', 'chilliwack', 'mission', 'kent', 'agassiz',
+        'harrison hot springs', 'hope',
+        'sechelt', 'gibsons',
+        'victoria', 'saanich', 'oak bay', 'esquimalt', 'view royal', 'colwood',
+        'langford', 'metchosin', 'highlands', 'sooke', 'central saanich', 'north saanich',
+        'sidney', 'duncan', 'north cowichan', 'lake cowichan', 'ladysmith', 'nanaimo',
+        'lantzville', 'parksville', 'qualicum beach', 'port alberni', 'tofino',
+        'ucluelet', 'courtenay', 'comox', 'cumberland', 'campbell river', 'gold river',
+        'tahsis', 'zeballos', 'port hardy', 'port mcneill', 'port alice', 'alert bay'
+    ];
+
+    function applyOwner(leadSourceId) {
+        if (!assignedField) {
+            return;
+        }
+
+        var leadSourceKey = leadSourceId ? String(leadSourceId) : '';
+
+        if (ownerMap.hasOwnProperty(leadSourceKey) && ownerMap[leadSourceKey]) {
+            assignedField.value = ownerMap[leadSourceKey];
+        } else {
+            assignedField.value = defaultAssignee;
+        }
+    }
+
+    function updateLeadSourceFromLocation() {
+        if (!leadSourceField || !stateField) {
+            return;
+        }
+
+        var provinceVal = stateField.value;
+
+        if (!provinceVal) {
+            leadSourceField.value = '';
+            applyOwner('');
+            return;
+        }
+
+        var leadSource = sourceMap.hasOwnProperty(provinceVal) ? String(sourceMap[provinceVal]) : '';
+
+        if (provinceVal === 'British Columbia') {
+            var cityVal = cityField ? cityField.value.trim().toLowerCase() : '';
+            for (var i = 0; i < bcCitiesKeywords.length; i++) {
+                if (cityVal.indexOf(bcCitiesKeywords[i]) !== -1) {
+                    leadSource = '2';
+                    break;
+                }
+            }
+        }
+
+        leadSourceField.value = leadSource;
+        applyOwner(leadSource);
+    }
+
+    if (stateField) {
+        stateField.addEventListener('change', updateLeadSourceFromLocation);
+    }
+
+    if (cityField) {
+        cityField.addEventListener('change', updateLeadSourceFromLocation);
+        cityField.addEventListener('keyup', updateLeadSourceFromLocation);
+    }
+
+    if (leadSourceField) {
+        leadSourceField.addEventListener('change', function () {
+            applyOwner(leadSourceField.value);
+        });
+    }
+
+    updateLeadSourceFromLocation();
+    if (leadSourceField) {
+        applyOwner(leadSourceField.value);
+    }
+})();
+</script>
